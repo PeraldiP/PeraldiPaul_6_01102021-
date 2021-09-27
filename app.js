d0ca21cbc34e97e3dbdb55ada507acd2f3e4c286
Peraldi <p.peraldi@epmistes.net>
2021-09-27 16:47:44 +0200

    Installation de Mongoose et connection à MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://peraldip:Kvgpmk1@projet6.6ryxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue 2 !' }); 
