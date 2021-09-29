dfd824669f6bb127ea1cd6b814d8e518d7f499b7
Peraldi <p.peraldi@epmistes.net>
2021-09-29 17:30:17 +0200

    Mise en place de multer

---
+++ middleware/multer-config.js
@@ -0,0 +1,20 @@
+const mutler = require ('multer');
+
+const MIME_TYPES = {
+    'images/jpg': 'jpg',
+    'images/jpeg':'jpg',
+    'images/png':'png'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file , callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPE[file.mimetype];
+        callback(null, name + Date.now() + '.' + extention);
+    }
+});
+
+module.exports = multer({ storage }).single('iamge');
