95ac183f243f1df654b91065fa80ffaff38dca89
Peraldi <p.peraldi@epmistes.net>
2021-09-29 14:40:24 +0200

    login utilisateur

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ mongoose.connect('mongodb+srv://peraldip:Kvgpmk1@projet6.6ryxb.mongodb.net/myFir
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue 2 !' }); 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
-
 
 app.post('/api/auth/signup',(req, res, next) => {
   bcrypt.hash(req.body.password, 10)
@@ -25,7 +27,7 @@ app.post('/api/auth/signup',(req, res, next) => {
       });
       user.save()
         .then(() => res.status(201).json({message: 'Utilisateur créé !'}))
-        .catch(error => res.status(400).json({error}));
+        .catch(error => res.status(500).json({error}));
 
     })
     .catch(error => res.status(500).json({error}));
@@ -34,11 +36,24 @@ app.post('/api/auth/signup',(req, res, next) => {
 });
 
 app.post('/api/auth/signup',(req, res, next) => {
-
+  User.findOne({ email : req.body.email})
+    .then(user => {
+      if(!user){
+        return res.status(401).json({error : 'Utilisateur non trouvé !'});
+      }
+      bcrypt.compare(req.body.password,user.password)
+        .then(valid => {
+          if(!valid){
+            return res.status(401).json({error : 'Mot de passe incorrecte !'});
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: 'TOKEN'
+          })
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 });
-
-
-
-
 
 module.exports = app;
