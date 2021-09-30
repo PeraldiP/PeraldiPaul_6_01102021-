766a9da4ac9a89e365bcd8cf77234c5edbaa8286
Peraldi <p.peraldi@epmistes.net>
2021-09-30 16:30:10 +0200

    Création des controllers pour les sauces

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+
 
 const sauceCtrl = require('../controllers/sauce');
 
-router.get('/',);
-router.get('/:id',);
-router.post('/',);
-router.put('/:id',);
-router.delete('/:id',);
-router.post('/:id',);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, sauceCtrl.createSauce);
+router.put('/:id', auth, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id', auth, sauceCtrl.likeDislikeSauce);
 
 module.exports = router;
