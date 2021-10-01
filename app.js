e6a63fedcdabe9357f0fab16ada176d99f246181
Peraldi <p.peraldi@epmistes.net>
2021-10-01 11:28:10 +0200

    Fonction Like Dislike

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use(express.json());
 app.use('/images',express.static(path.join(__dirname,'images')));
 
 app.use('/api/auth', userRoutes);
-app.use('api/sauce', sauceRoutes);
+app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,9 +1,9 @@
-const multer = require ('multer');
+const multer = require('multer');
 
 const MIME_TYPES = {
-    'images/jpg': 'jpg',
-    'images/jpeg':'jpg',
-    'images/png':'png'
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
 };
 
 const storage = multer.diskStorage({
@@ -12,8 +12,8 @@ const storage = multer.diskStorage({
     },
     filename: (req, file , callback) => {
         const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPE[file.mimetype];
-        callback(null, name + Date.now() + '.' + extention);
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
     }
 });
 
